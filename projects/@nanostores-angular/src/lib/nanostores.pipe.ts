338952258df02cbc48aa81c6f7334f2fc12f8a16
infodusha <infodusha@ya.ru>
2023-02-05 17:38:18 +0300

    fix: cache observable

--- projects/@nanostores-angular/src/lib/nanostores.pipe.ts
+++ projects/@nanostores-angular/src/lib/nanostores.pipe.ts
@@ -1,20 +1,34 @@
 import { ChangeDetectorRef, Pipe, PipeTransform } from "@angular/core";
 import { Store } from "nanostores";
 import { AsyncPipe } from "@angular/common";
-import {observeStore} from "./observe-store.helper";
+import { observeStore } from "./observe-store.helper";
+import { Observable } from "rxjs";
 
 @Pipe({
     name: 'stored',
     pure: false,
 }) export class NanostoresPipe<T> implements PipeTransform, Omit<AsyncPipe, 'transform'> {
-    asyncPipe: AsyncPipe;
+    private asyncPipe: AsyncPipe;
+    private lastValue: Observable<T> | null = null;
+    private lastStore: Store<T> | null = null;
 
     constructor(ref: ChangeDetectorRef) {
         this.asyncPipe = new AsyncPipe(ref);
     }
 
     transform(store: Store<T> | null | undefined): T | null {
-        return this.asyncPipe.transform(store ? observeStore<T>(store) : null);
+        if (!store) {
+            this.lastValue = null;
+            this.lastValue = null;
+            return null;
+        }
+
+        if (this.lastStore !== store) {
+            this.lastStore = store;
+            this.lastValue = observeStore<T>(store);
+        }
+
+        return this.asyncPipe.transform(this.lastValue);
     }
 
     ngOnDestroy(): void {
--- test/example/current-user-async-pipe/current-user-async-pipe.component.ts
+++ test/example/current-user-async-pipe/current-user-async-pipe.component.ts
@@ -5,11 +5,22 @@ import { profile } from '../stores/profile';
 
 @Component({
   selector: 'app-current-user-async-pipe',
-  template: '<p *ngIf="(getUser(userId | stored) | stored) as user">{{ user.name }}</p>'
+  template: `
+    <p *ngIf="(getUser(userId | stored) | stored) as user">{{ user.name }}</p>
+    <div>{{ profile | stored | json }}</div>
+  `
 })
 export class CurrentUserAsyncPipeComponent {
 
+  profile = profile;
+
   userId = computed(profile, ({ userId }) => userId)
+
+  constructor() {
+    setInterval(() => {
+      profile.set({ userId: Math.random().toString(), name: 'John' });
+    }, 1000);
+  }
 
   getUser(userId: string) {
     return User(userId);
